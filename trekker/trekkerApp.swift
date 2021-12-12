09442df8dd0ad98799a935ffcbfd60c215a9abf3
Ana Luiza Lisboa <analu.lisboa@arctouch.com>
2021-12-12 08:27:39 -0300

    Initial Commit

---
+++ trekker/trekkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trekkerApp.swift
+//  trekker
+//
+//  Created by Analu Lisboa on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct trekkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
